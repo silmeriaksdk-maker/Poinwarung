e6bab8cdef8b92123b8e1fe2dfebd00dc77b7898
silmeriaksdk-maker <silmeria.ksdk@gmail.com>
2026-06-18 17:35:12 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'mabirh-poin-v2';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
+];
+
+// Install Service Worker & Simpan Aset ke Cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+// Aktivasi & Hapus Cache Lama jika ada pembaruan sistem
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Strategi Stale-While-Revalidate (Buka dari cache dulu, update di background)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        fetch(event.request).then((networkResponse) => {
+          if (networkResponse.status === 200) {
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, networkResponse));
+          }
+        }).catch(() => { /* Abaikan jika offline */ });
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
